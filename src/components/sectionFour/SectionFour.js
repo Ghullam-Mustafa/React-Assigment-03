5635d0a851477ea4506382f0c4d26013194200c8
Ghullam Mustafa <gmustafawaince180@gmail.com>
2023-07-17 01:18:41 +0500

    section four

---
+++ src/components/sectionFour/SectionFour.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function SectionFour() {
+  return (
+    <div class="container">
+
+    <div class="container d-flex justify-content-center">
+      <button type="button" class="btn btn-dark btn-lg rounded-0">Read More</button>
+    </div>
+
+  </div>
+  )
+}
--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -5,15 +5,17 @@ import Navbar from '../components/navbar/Navbar';
 import SectionOne from '../components/sectionOne/SectionOne';
 import SectionTwo from '../components/sectionTwo/SectionTwo';
 import SectionThree from '../components/sectionThree/SectionThree';
+import SectionFour from '../components/sectionFour/SectionFour';
 
 
 export default function MainPage(props) {
   return (
     <>
     <Navbar />
-    <SectionOne />
-    <SectionTwo />
+    <SectionOne/>
+    <SectionTwo/>
     <SectionThree/>
+    <SectionFour/>
     </>
     
   )
